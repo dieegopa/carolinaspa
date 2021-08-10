a6e7048165ec6316799d3fa7f08573c2690a663b
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-08-10 22:15:43 +0200

    Update conexion.php

--- Carolina Spa/includes/conexion.php
+++ Carolina Spa/includes/conexion.php
@@ -1,8 +1,8 @@
 <?php
-  $host_name_db = 'sql11.freemysqlhosting.net';
-  $database_db = 'sql11393353';
-  $user_name_db = 'sql11393353';
-  $password_db = 'cV1lAxe2A3';
+  $host_name_db = 'HOSTING';
+  $database_db = 'NAME';
+  $user_name_db = 'USERNAME';
+  $password_db = 'PASSWORD';
 
   $conexion = new mysqli($host_name_db, $user_name_db, $password_db, $database_db);
 
